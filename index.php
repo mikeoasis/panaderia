ea7eaf3646a1d432669d25929310cd14756e2c83
Miguel Settecasse <miguelsettecasse1@Miguels-MacBook-Pro.local>
2015-09-20 23:32:23 -0300

    Commit message

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
-<?php 
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Panaderia</title>
+<!-- Latest compiled and minified CSS -->
+ <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+<!-- Optional theme -->
+<link  href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
+</head>
 
-echo "Hola guachiturro...................................................";
-echo "caca";
-
+<body>
+  <div class="bs-example" data-example-id="simple-nav-tabs">
+    <ul class="nav nav-tabs">
+      <li role="presentation" class="active"><a href="#">Home</a></li>
+      <li role="presentation"><a href="#">Profile</a></li>
+      <li role="presentation"><a href="#">Messages</a></li>
+    </ul>
+  </div>
+<script src="js/jquery-1.11.3.js"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
